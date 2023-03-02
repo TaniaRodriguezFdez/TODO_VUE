04ad4cc94053667491e6c82cad1b53be81e97a7f
Tania Rodríguez Fernández <taniacsif@gmail.com>
2023-03-02 22:54:13 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+new Vue({
+  el: "#taskList",
+  data: {
+    title: "to do list",
+          tasks: [
+                  {
+                          name: 'Today: international meeting'
+                  },
+
+    ],
+  },
+  methods: {
+    newItem: function () {
+      if (!this.tasks.name) {
+        return
+      }
+      this.tasks.push({
+        name: this.tasks.name,
+        del: "",
+      });
+      this.tasks.name = "";
+          },
+          delItem: function (task) {
+              this.tasks.splice(this.tasks.indexOf(task), 1)    
+        }
+                  
+
+    
+  }
+});
